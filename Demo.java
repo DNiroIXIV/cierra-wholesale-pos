320af6c053ca3e3dc09fb2d8f933977c948e4e72
DNiroIXIV <dnirodhaixiv@gmail.com>
2025-04-16 03:31:10 +0530

    commit04

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 public class Demo {
     public static void main(String[] args) {
-        double a = 24, b = 0, c;
-
+        int a = 3, b = 0, c;
+        int[] ar = new int[3];
         System.out.println("Start Main");
-        c = a/b; 
-        System.out.println("c = "+c);
+        try {
+            ar[a] = 100;    
+        } catch (ArrayIndexOutOfBoundsException e) {
+            ar = new int[a+1];
+        }        
         System.out.println("End Main");
     }    
 }
-
