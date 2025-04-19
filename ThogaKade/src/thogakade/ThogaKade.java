945dada1a462d1698afc229224de8a9fa388c205
DNiroIXIV <dnirodhaixiv@gmail.com>
2025-04-19 09:07:17 +0530

    commit12

--- ThogaKade/src/thogakade/ThogaKade.java
+++ ThogaKade/src/thogakade/ThogaKade.java
@@ -6,11 +6,18 @@ public class ThogaKade {
 
     public static void main(String[] args) {
         try {
-            String SQL = "insert into customer values('C111', 'Niroth', 'Panadura', 60000)";
+            String SQL = "select * from customer";
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/thogakade", "root", "12345");
             Statement statement = connection.createStatement();
-            statement.executeUpdate(SQL);
+            ResultSet resultSet = statement.executeQuery(SQL);
+            
+            resultSet.next();
+            String id = resultSet.getString("id");
+            String name = resultSet.getString("name");
+            String address = resultSet.getString("address");
+            double salary = resultSet.getDouble("salary");
+            System.out.println(id + "\t" + name + "\t" + address + "\t" + salary);
         } catch (ClassNotFoundException ex) {
             System.out.println("Driver software not found");
         } catch (SQLException ex) {
